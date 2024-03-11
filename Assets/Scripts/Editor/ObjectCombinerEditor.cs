c669098ab1a508fa69a8c78a1a5189e73ee8d277
beatHunteRcode <nikitasherepa@gmail.com>
2024-03-11 21:16:34 +0300

    Added HelpBoxes to FoldOut groups in the Inspector

--- Assets/Scripts/Editor/ObjectCombinerEditor.cs
+++ Assets/Scripts/Editor/ObjectCombinerEditor.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Experimental.AI;
 using UnityEngine.UIElements;
 
 [CustomEditor(typeof(ObjectCombiner))]
@@ -34,6 +35,12 @@ public class ObjectCombinerEditor : Editor
 
         if (isCombineObjectsByPolygonsGroupOpened && Selection.activeTransform)
         {
+            EditorGUILayout.HelpBox("Combines all objects on scene if sum of all objects polygons (triangles) are greater than Polygons limit\n" +
+                "1. Enter polygons limit\n" +
+                "2. Hit \"Combine Objects by Polygons\" button", 
+                MessageType.Info);
+
+
             objectCombiner.trianglesLimit = EditorGUILayout.IntField("Polygons limit", objectCombiner.trianglesLimit);
 
             if (GUILayout.Button("Combine Objects by Polygons"))
@@ -55,6 +62,10 @@ public class ObjectCombinerEditor : Editor
 
         if (isCombineObjectsByMaterialsGroupOpened && Selection.activeTransform)
         {
+            EditorGUILayout.HelpBox("All objects with specific materials will combine in one GameObject\n" +
+                "1. Hit \"Combine Objects by Materials\" button",
+                MessageType.Info);
+
             if (GUILayout.Button("Combine Objects by Materials"))
             {
                 objectCombiner.CombineObjectsByMaterials();
@@ -73,6 +84,12 @@ public class ObjectCombinerEditor : Editor
 
         if (isCombineObjectsByTagsGroupOpened && Selection.activeTransform)
         {
+            EditorGUILayout.HelpBox("All objects with specific tag will combine in one GameObject\n" +
+                "1. Enter amount of Tags to combine\n" +
+                "2. Select each specific Tag\n" +
+                "3. Hit \"Combine Objects by Tags\" button",
+                MessageType.Info);
+
             SerializedProperty tagsToCombineProperty = serializedObject.FindProperty("tagsToCombine");
             EditorGUILayout.PropertyField(tagsToCombineProperty, new GUIContent("Tags to Combine"));
 
@@ -94,6 +111,13 @@ public class ObjectCombinerEditor : Editor
 
         if (isCombineObjectsByDistanceGroupOpened && Selection.activeTransform)
         {
+            EditorGUILayout.HelpBox("Combines all child-objects in specific GameObject-collection if distances between all chils-objects pivots in collection are greater than Distance limit\n" +
+                "1. Enter Distance limit\n" +
+                "2. Enter amount of collections to work with\n" +
+                "3. Select each specific collection (or Drag'n'Drop )\n" +
+                "3. Hit \"Combine Objects by Distance\" button",
+                MessageType.Info);
+
             objectCombiner.distanceLimit = EditorGUILayout.FloatField("Disnance limit", objectCombiner.distanceLimit);
 
             SerializedProperty collectionOfObjectsToCombineByDistance = serializedObject.FindProperty("collectionOfObjectsToCombineByDistance");
